c74c21c146971f46ebad399ce06af415dd587f40
Samsuddin Asliwala <Samsupatel02@gmail.com>
2025-02-19 19:45:30 +0530

    BlobService Logic Implement

---
+++ AzureFundamentals/Services/BlobService.cs
@@ -0,0 +1,68 @@
+﻿
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzureFundamentals.Services
+{
+    public class BlobService : IBlobService
+    {
+        private readonly BlobServiceClient _blobClient;
+
+        public BlobService(BlobServiceClient blobClient)
+        {
+            _blobClient = blobClient;
+        }
+        public async Task<bool> DeleteBlob(string name, string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+
+            var blobClient = blobContainerClient.GetBlobClient(name);
+
+            return await blobClient.DeleteIfExistsAsync();
+        }
+
+        public async Task<List<string>> GetAllBlobs(string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            var blobs = blobContainerClient.GetBlobsAsync();
+
+            var blobString = new List<string>();
+
+            await foreach (var item in blobs)
+            {
+                blobString.Add(item.Name);
+            }
+
+            return blobString;
+        }
+
+        public async Task<string> GetBlob(string name, string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+
+            var blobClient = blobContainerClient.GetBlobClient(name);
+
+            return blobClient.Uri.AbsoluteUri;
+        }
+
+        public async Task<bool> UploadBlob(string name, IFormFile file, string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+
+            var blobClient = blobContainerClient.GetBlobClient(name);
+
+            var httpHeaders = new BlobHttpHeaders()
+            {
+                ContentType = file.ContentType
+            };
+
+            var result = await blobClient.UploadAsync(file.OpenReadStream(), httpHeaders);
+
+            if (result != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ AzureFundamentals/Services/IBlobService.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureFundamentals.Services
+{
+    public interface IBlobService
+    {
+        Task<string> GetBlob(string name, string containerName);
+        Task<List<string>> GetAllBlobs(string containerName);
+        Task<bool> UploadBlob(string name, IFormFile file, string containerName);
+        Task<bool> DeleteBlob(string name, string containerName);
+    }
+}
